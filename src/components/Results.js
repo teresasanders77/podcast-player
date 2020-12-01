9e3cd165159eddc368267bb3950dd16242a97433
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-01 13:19:36 -0700

    Working on implementing router

---
+++ src/components/Results.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+
+
+  render() {
+    return (
+      <div>
+        It worked!
+      </div>
+
+    );
+  }
+}
+
+export default Results;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { listenNotesApi } from '../axios'
-import Logo from './Icons/Logo.js'
-import SearchBarItem from './SearchBarItem'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Redirect } from 'react-router'
+import { listenNotesApi } from '../axios';
+import Logo from './Icons/Logo.js';
+import { SearchBarItem } from './SearchBarItem';
+import Results from './Results';
 
 export const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('')
@@ -26,35 +29,48 @@ export const SearchBar = () => {
     }
   }, [searchTerm])
 
+  function handleSubmit(event) {
+    if (searchResults > 0) {
+      <Redirect to="/results/:searchResults" />
+    }
+
+  }
+
   return (
     <div className="SearchBar">
-      <div className="logo">
-        <Logo width="70pt" />
-      </div>
-      <h2>Welcome!</h2>
-      <h1>Explore top podcast!</h1>
-      <input
-        type="text"
-        className="podcast"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-        placeholder="Search podcasts"
-      />
-      {searchTerm && (
-        <div>
-          <ul>
-            {isLoading}
-            {searchResults &&
-              searchResults.map(podcast => (
-                <SearchBarItem
-                  clearSearch={() => setSearchTerm('')}
-                  key={podcast.id}
-                  {...podcast}
-                />
-              ))}
-          </ul>
+      <Router>
+        <Router exact path="/" render={() => <SearchBar />} />
+        <div className="logo">
+          <Logo width="70pt" />
         </div>
-      )}
+        <h2>Welcome!</h2>
+        <h1>Explore top podcast!</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="podcast"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            placeholder="Search podcasts"
+          />
+        </form>
+        <Router exact path="/results/:searchResults" render={() => <Results />} />
+        {searchTerm && (
+          <div>
+            <ul>
+              {isLoading}
+              {searchResults &&
+                searchResults.map(podcast => (
+                  <SearchBarItem
+                    clearSearch={() => setSearchTerm('')}
+                    key={podcast.id}
+                    {...podcast}
+                  />
+                ))}
+            </ul>
+          </div>
+        )}
+      </Router>
     </div>
   )
 }
