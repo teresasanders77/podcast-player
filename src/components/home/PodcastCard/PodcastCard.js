ea86358c3716aa1f23e9cb59a93f46d2f83a3fc4
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-03 16:31:27 -0700

    Added audio player and accordion for cards

--- src/components/home/PodcastCard/PodcastCard.js
+++ src/components/home/PodcastCard/PodcastCard.js
@@ -1,30 +1,53 @@
 import React, { Component } from 'react'
+import AudioPlayer from 'react-h5-audio-player';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
+import 'react-h5-audio-player/lib/styles.css';
 import './PodcastCard.css';
 
 export class PodcastCard extends Component {
 
   render() {
+    const Player = () => (
+      <AudioPlayer
+        autoPlay
+        src={podcast.audio}
+        onPlay={e => console.log("onPlay")}
+      // other props here
+      />
+    )
     const { podcast } = this.props;
     return (
       <div id="container">
-        <div className="card mb-1">
-          <div className="row h-100 no-gutters">
-            <div className="col-md-3">
-              <img className="w-100 img-fluid" src={podcast.thumbnail} alt="Podcast Img" />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">
-                  {podcast.title_original}
-                </h5>
-                <p className="card-text" dangerouslySetInnerHTML={{ __html: podcast.description_highlighted }} />
-                <a className="btn btn-primary" href="#">
-                  Play Podcast
-              </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                <div className="col-md-3">
+                  <img className="w-100 img-fluid" src={podcast.thumbnail} alt="Podcast Img" />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {podcast.title_original}
+                    </h5>
+                    <p className="card-text" dangerouslySetInnerHTML={{ __html: podcast.description_highlighted }} />
+                  </div>
+                </div>
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <AudioPlayer
+                  src={podcast.audio}
+                  onPlay={e => console.log("onPlay")}
+                // other props here
+                />
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       </div>
     )
   }
