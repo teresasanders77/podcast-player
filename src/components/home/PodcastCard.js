b1cf8e3f3b3e0e302cbf4a9d2c737d19f54b444a
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-02 20:08:32 -0700

    Begin styling results page

--- src/components/home/PodcastCard.js
+++ src/components/home/PodcastCard.js
@@ -4,17 +4,23 @@ export class PodcastCard extends Component {
   render() {
     const { podcast } = this.props;
     return (
-      <div>
-        <div className="col-md-3 mb-5">
-          <div className="card card-body bg-dark text-center h-100">
-            <img className="w-100 mb-2" src={podcast.image} alt="Podcast Img" />
-            <h5 className="text-light card-title">
-              {podcast.title_original} - {podcast.publisher_original}
-            </h5>
-            <a className="btn btn-primary" href="#">
-              Play Podcast
-              <i className="fas fa-chevron-right" />
-            </a>
+      <div id="container">
+        <div className="card mb-1">
+          <div className="row h-100 no-gutters">
+            <div className="col-md-3">
+              <img className="w-100 img-fluid" src={podcast.image} alt="Podcast Img" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">
+                  {podcast.title_original}
+                </h5>
+                <p className="card-text">{podcast.publisher_original}</p>
+                <a className="btn btn-primary" href="#">
+                  Play Podcast
+              </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
