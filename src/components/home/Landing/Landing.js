586ab17b34279b1c9d8f4a967b623dc76a31896c
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-07 16:34:02 -0700

    Fixed background in results page

--- src/components/home/Landing/Landing.js
+++ src/components/home/Landing/Landing.js
@@ -7,20 +7,30 @@ import './Landing.css';
 
 
 export class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      podcasts: []
+    }
+  }
   render() {
-    const { loading } = this.props;
-    return (
-      <div>
-        <h2>Welcome!</h2>
-        <h1>Explore top podcasts!</h1>
-        <SearchForm />
-        {loading ? <Spinner /> : <PodcastsContainer />}
-      </div>
-    )
+    const { podcasts } = this.props;
+    if (podcasts === undefined) {
+      return (
+        <div>
+          <h2>Welcome!</h2>
+          <h1>Explore top podcasts!</h1>
+          <SearchForm />
+        </div>
+      )
+    } else {
+      return <PodcastsContainer />;
+    }
   }
 }
 
 const mapStateToProps = state => ({
-  loading: state.podcasts.loading
+  loading: state.podcasts.loading,
+  podcasts: state.podcasts.podcasts.results,
 })
 export default connect(mapStateToProps)(Landing); 
--- src/components/home/PodcastsContainer/PodcastsContainer.js
+++ src/components/home/PodcastsContainer/PodcastsContainer.js
@@ -2,7 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import PodcastCard from "../PodcastCard/PodcastCard";
 import './PodcastsContainer.css';
-import BackButton from '../../Icons/BackButton'
+import BackButton from '../../Icons/BackButton';
+import SearchForm from '../Landing/Landing';
 
 
 
