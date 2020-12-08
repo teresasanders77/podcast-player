f6819ddaee4d268abd3d10501a904efc2eabcf18
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-08 16:14:45 -0700

    Cleaned up code- App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
-import Logo from './components/Icons/Logo.js'
 import { Route, Switch } from 'react-router-dom'
+
+import Logo from './components/Icons/Logo.js'
 import Landing from './components/home/SearchForm/SearchForm';
 import SearchForm from './components/home/Landing/Landing';
 import PodcastsContainer from './components/home/PodcastsContainer/PodcastsContainer';
 import APILogo from './apiLogo.png';
 
-
+import './App.css';
 import store from './store';
 
 
 class App extends Component {
+
   render() {
 
     return (
+      /*App Logos, store/routes*/
       <div className="App">
         <div className="logo">
-          <Logo width="70pt" />
+          <a href="/"><Logo width="70pt" /></a>
         </div>
         <Provider store={store}>
           <SearchForm>
             <Switch>
               <Route exact path='/' component={Landing} />
               <div className="podcastsContainer">
-                <Route
-                  path='/results'
-                  component={PodcastsContainer}
-                />
+                <Route path='/results' component={PodcastsContainer} />
               </div>
             </Switch>
           </SearchForm>
         </Provider>
         <div className="apiLogo">
-          <a href="https://www.listennotes.com/" target="_blank" rel="ListenNotes.com"> <img src={APILogo} alt="API logo" /></a>
+          <a href="https://www.listennotes.com/" target="_blank" rel="noreferrer"> <img src={APILogo} alt="API logo" /></a>
         </div>
       </div>
     );
