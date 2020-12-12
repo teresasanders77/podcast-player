891c496a0b542edd6b5f626053aebdde509b01b9
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-11 19:26:43 -0700

    Refresh now working, need to fix routing issue for back

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Logo from './components/Icons/Logo.js'
 import Landing from './components/home/SearchForm/SearchForm';
 import SearchForm from './components/home/Landing/Landing';
 import PodcastsContainer from './components/home/PodcastsContainer/PodcastsContainer';
-
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import './App.css';
-import store from './store';
 
 
 const App = () => {
@@ -22,14 +22,16 @@ const App = () => {
         <a href="/"><Logo width="70pt" /></a>
       </div>
       <Provider store={store}>
-        <SearchForm>
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <div className="podcastsContainer">
-              <Route path='/results' component={PodcastsContainer} />
-            </div>
-          </Switch>
-        </SearchForm>
+        <PersistGate persistor={persistor}>
+          <SearchForm>
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <div className="podcastsContainer">
+                <Route path='/results' component={PodcastsContainer} />
+              </div>
+            </Switch>
+          </SearchForm>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,23 +1,31 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
-function saveToLocalStorage(state) {
-  try {
-    const serializedState = JSON.stringify(state)
-    localStorage.setItem('state', serializedState)
-  } catch (e) {
-    console.log(e)
-  }
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel2
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+const configureStore = (initialState) => {
+  return createStore(
+    pReducer,
+    initialState,
+    composeEnhancers(
+      applyMiddleware(thunk)
+    )
+  )
 }
 
-const middleware = [thunk];
-const initialState = {};
-
-const store = createStore(rootReducer, initialState, composeWithDevTools
-  (applyMiddleware(...middleware)));
-
-store.subscribe(() => saveToLocalStorage(store.getState()))
-
-export default store;
+export const store = configureStore()
+export const persistor = persistStore(store);
