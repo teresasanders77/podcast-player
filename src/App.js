305def87f7de627067b10aa5c63a1f0cc377055d
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-12-13 12:13:50 -0700

    Revert "Refresh now working, need to fix routing issue for back"
    This reverts commit 891c496a0b542edd6b5f626053aebdde509b01b9.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Logo from './components/Icons/Logo.js'
 import Landing from './components/home/SearchForm/SearchForm';
 import SearchForm from './components/home/Landing/Landing';
 import PodcastsContainer from './components/home/PodcastsContainer/PodcastsContainer';
-import { store, persistor } from './store';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+
 
 import './App.css';
+import store from './store';
 
 
 const App = () => {
@@ -22,16 +22,14 @@ const App = () => {
         <a href="/"><Logo width="70pt" /></a>
       </div>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <SearchForm>
-            <Switch>
-              <Route exact path='/' component={Landing} />
-              <div className="podcastsContainer">
-                <Route path='/results' component={PodcastsContainer} />
-              </div>
-            </Switch>
-          </SearchForm>
-        </PersistGate>
+        <SearchForm>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <div className="podcastsContainer">
+              <Route path='/results' component={PodcastsContainer} />
+            </div>
+          </Switch>
+        </SearchForm>
       </Provider>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,31 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
-  compose
-
-const persistConfig = {
-  key: 'root',
-  storage: storage,
-  stateReconciler: autoMergeLevel2
-};
-
-const pReducer = persistReducer(persistConfig, rootReducer);
-
-const configureStore = (initialState) => {
-  return createStore(
-    pReducer,
-    initialState,
-    composeEnhancers(
-      applyMiddleware(thunk)
-    )
-  )
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-export const store = configureStore()
-export const persistor = persistStore(store);
+const middleware = [thunk];
+const initialState = {};
+
+const store = createStore(rootReducer, initialState, composeWithDevTools
+  (applyMiddleware(...middleware)));
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
+
+export default store;
